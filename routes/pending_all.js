24006f5a8fc3baa1d3a8175f6a3090b7c88c295b
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-11-12 13:06:23 +0300

    .

--- routes/pending_all.js
+++ routes/pending_all.js
@@ -3,23 +3,26 @@ const Adress = require("../models/adress");
 const Another = require("../models/anothe");
 const Hall = require("../models/hall");
 const Farm = require("../models/farm");
+const Tourism = require("../models/tourism");
 const router = express.Router();
 
 
 router.get("/pending-all",async (req, res) => {
     try {
-      const [pendingAdresses, pendingAnothers, pendingHalls, pendingFarms] = await Promise.all([
+      const [pendingAdresses, pendingAnothers, pendingHalls, pendingFarms, pendingTourism] = await Promise.all([
         Adress.findAll({ where: { status: 'pending' } }),
         Another.findAll({ where: { status: 'pending' } }),
         Hall.findAll({ where: { status: 'pending' } }),
         Farm.findAll({ where: { status: 'pending' } }),
+        Tourism.findAll({ where: { status: 'pending' } }),
       ]);
   
       const pendingItems = [
         ...pendingAdresses.map(item => ({ ...item.dataValues, type: 'adress' })),
         ...pendingAnothers.map(item => ({ ...item.dataValues, type: 'another' })),
         ...pendingHalls.map(item => ({ ...item.dataValues, type: 'hall' })),
-        ...pendingFarms.map(item => ({ ...item.dataValues, type: 'farm' }))
+        ...pendingFarms.map(item => ({ ...item.dataValues, type: 'farm' })),
+        ...pendingTourism.map(item => ({ ...item.dataValues, type: 'tourism' })),
       ];
   
       res.status(200).json(pendingItems);
